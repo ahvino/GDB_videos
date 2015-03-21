4b90f1fb2ee48f2a4d4c4724dc10b5e5859ac180
Selik <seliksamai@gmail.com>
2015-03-20 18:57:56 -0700

    final changes

--- ex1.cpp
+++ ex1.cpp
@@ -10,19 +10,11 @@
 
 using namespace std;
 
-int factorial(int x){
-//This is going to find the factorial of a number
-     int y;
-     if(x <= 1){ return 1;}
-          y = x* factorial(x-1);
-          return y;
-}
-
 void segfault1(){
-//This program produces a very
+//This produces a very
 //common segfault
 
-     //how big am I?
+     //how big is the array?
      int ten[10];
      int twenty = 20;
 
@@ -36,30 +28,17 @@ int segfault2(int x){
 //This also produces a segfault
 //because there's no base case
 //to end the recursive calls
-//where's the base case yo?
-     if( x >= 1) return 0;
+     //if(x >= 1) return 0;
 
      return segfault2(x-1);
-
 }
 
-void simple_fork(){
-//This is a simple use case
-//of the sys call fork
-//child and parent are
-//separate
-
-     int pid = fork();
-     if(pid == -1){ perror("error in fork");}
-
-     else if(pid == 0){
-          cout <<"CHILD says 'I am a child' "<< endl;
-          exit(0);
-     }
-     else if(pid > 0){
-          wait(0);
-          cout <<"PARENT says 'I am your parent' " << endl;
-     }
+int factorial(int x){
+//This is going to find the factorial of a number
+     int y;
+     if(x <= 1){ return 1;}
+          y = x* factorial(x-1);
+          return y;
 }
 
 int initialize(){
@@ -91,57 +70,53 @@ bool leap_year(){
      }
 }
 
-void mysort(){
+void my_max(){
 //This is going to sort some variables
 //Unfortunately, none of the variables have been
 //initialized...ouch!
 
-     int first, second, third;
-     int a,b,c;
+     int first, second;
+     int a,b;
 
-     if(a > b && a > c){
+     if(a > b ){
           first = a;
-          if(b > c) {
-               second = b;
-               third = c;
-          }
-          else {
-               second = c;
-               third = b;
-          }
-          cout <<"first:  "<<  first << endl;
-          cout <<"second: "<< second << endl;
-          cout <<"third:  "<< third << endl;
+          second = b;
      }
-     if(b > a && b > c){
+     else if(a < b) {
           first = b;
-          if(a > c) {
-               second = a;
-               third = c;
-          }
-          else {
-               second = c;
-               third = a;
-          }
-          cout <<"first:  "<<  first << endl;
-          cout <<"second: "<< second << endl;
-          cout <<"third:  "<< third << endl;
+          second = a;
      }
-     if(c > b && c > c){
-          first = c;
-          if(b > a) {
-               second = b;
-               third = a;
-          }
-          else {
-               second = a;
-               third = b;
-          }
-          cout <<"first:  "<<  first << endl;
-          cout <<"second: "<< second << endl;
-          cout <<"third:  "<< third << endl;
+     else if(a == b) {cout << "You GDB often?" << endl;}
+
+     cout <<"first:  "<<  first << endl;
+     cout <<"second: "<< second << endl;
+}
+
+void simple_fork(){
+//This is a simple use case
+//of the sys call fork
+//child and parent are
+//separate
+
+     int pid = fork();
+     if(pid == -1){ perror("error in fork");}
+
+     else if(pid == 0){
+          cout <<"CHILD says 'I am a child' "<< endl;
+          leap_year();
+          exit(0);
+          //set follow-fork-mode to see what's happening
+     }
+     else if(pid > 0){
+          wait(0);
+          cout <<"\nPARENT says 'I am your parent' " << endl;
+          cout <<"Enter a number and I'll get the factorial: ";
+          int x;
+          cin >> x;
+          cout << factorial(x) << endl;
      }
 }
+
 
 int main(){
 
@@ -150,56 +125,56 @@ while (1){
      int number;
 
      cout <<"\nEnter 0 to quit" << endl;
-     cout <<"Enter 1 to find a factorial" << endl;
-     cout <<"Enter 2 to look at an unitialized var " << endl;
-     cout <<"Enter 3 to run sort on unitialized var's" << endl;
-     cout <<"Enter 4 to find out if a year is a leap year" << endl;
-     cout <<"Enter 5 to see a segfault " << endl;
-     cout <<"Enter 6 to see another common segfault" << endl;
+     cout <<"Enter 1 to see a segfault " << endl;
+     cout <<"Enter 2 to see another common segfault" << endl;
+     cout <<"Enter 3 to find a factorial" << endl;
+     cout <<"Enter 4 to look at an unitialized var " << endl;
+     cout <<"Enter 5 to find out if a year is a leap year" << endl;
+     cout <<"Enter 6 to run max on unitialized var's" << endl;
      cout <<"Enter 7 to see some processes" << endl;
-     cout << "Enter a number and we'll run a function: ";
+     cout << endl;
+     cout <<"Enter a number and we'll run a function: ";
      cin >> number;
 
      cout << endl;
      cout << endl;
      if(number == 0) {return 0;}
      if(number == 1){
+//          cout <<"This will produce a segfault" << endl;
+          segfault1();
+     }
+     if(number == 2){
+//          cout <<"This too will produce a segfault" << endl;
+          segfault2(10);
+     }
+     if(number == 3){
           cout << "enter another number: ";
           int x;
           cin >> x;
-          cout << "let's find the factorial of " << x << endl;
+          cout << "Finding the factorial of " << x << endl;
           cout <<"The facorial of " << x <<" is: ";
           cout << factorial(x) << endl;
 
      }
-     if(number == 2){
-          cout <<"here we'll call a function that will do some math";
+     if(number == 4){
+          cout <<"Here we'll call a function that will do some math";
           cout <<" without first initializing" << endl;
           cout << initialize() << endl;
      }
-     if(number == 3){
-          cout <<"here are some variables that haven't been initialized ";
-          cout <<"but we'll do something with them anyways" << endl;
-          cout << initialize() << endl;
-          mysort();
-     }
-     if(number == 4){
-          leap_year();
-     }
 
      if(number == 5){
-//          cout <<"This will produce a segfault" << endl;
-          segfault1();
+          leap_year();
      }
      if(number == 6){
-//          cout <<"This too will produce a segfault" << endl;
-          segfault2(10);
+          cout <<"Here are some variables that haven't been initialized ";
+          cout <<"but we'll do something with them anyways" << endl;
+
+          my_max();
      }
      if(number == 7){
           cout <<"Let's use a system call and see some processes" << endl;
           simple_fork();
      }
-     cout << endl;
      cout << endl;
      }
 
